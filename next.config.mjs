1ad0b1b8b0c6b8422e3d401dfb1d865edd375eb0
charosolis1 <csolis@unsam.edu.ar>
2025-04-02 00:33:25 -0300

    Corregir rutas para CSS y JS

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Descomenta y reemplaza con el nombre de tu repositorio cuando lo subas a GitHub
   basePath: '/proyecto-x-encefalomielitis',
   assetPrefix: '/proyecto-x-encefalomielitis/',
   trailingSlash: true,
